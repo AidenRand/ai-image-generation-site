a754c03a475ebd91f38d395b66bcfdb6ce3a3d58
Cortrax <aidenrphotos@gmail.com>
2023-10-03 22:26:51 -0400

    Add createImage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,39 @@
 import React, { useRef } from 'react';
 import '../styling/home.scss';
 import SubmitIcon from '../assets/submit.png';
+import OpenAI from 'openai';
+import createImage from 'openai';
+
+async function getImage(user_input) {
+    const openai = new OpenAI({
+        apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+        dangerouslyAllowBrowser: true,
+    });
+
+    const response = await openai.createImage({
+        prompt: user_input,
+        n: 1,
+        size: '1024x1024',
+    });
+
+    console.log(response);
+}
 
 function Home() {
     const inputRef = useRef(0);
+
+    const handleInput = async () => {
+        const user_input = inputRef.current.value;
+
+        try {
+            const response = getImage(user_input);
+            console.log(response);
+        } catch (err) {
+            console.log('There was an error with the open ai call', err);
+        }
+
+        inputRef.current.value = '';
+    };
     return (
         <div className='home-container'>
             <div className='input-container'>
@@ -15,7 +45,11 @@ function Home() {
                 ></input>
 
                 <button className='submit-btn'>
-                    <img src={SubmitIcon} alt='submit-icon' />
+                    <img
+                        src={SubmitIcon}
+                        alt='submit-icon'
+                        onClick={handleInput}
+                    />
                 </button>
             </div>
         </div>
